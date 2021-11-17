e66ab6a7632dcba1bc765ac5a9be08ccef0c1626
Horange <horange1024@outlook.com>
2021-11-16 22:47:47 -0600

    add

---
+++ Angry Blox - Peer Grading/Assets/Code/Bomb.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+    public float ThresholdForce = 2;
+    public GameObject ExplosionPrefab;
+
+    void Destruct() {
+      Destroy(gameObject);
+    }
+
+    void Boom() {
+      PointEffector2D explosionEff = gameObject.GetComponent<PointEffector2D>();
+      explosionEff.enabled = true;
+      SpriteRenderer spriteRen = gameObject.GetComponent<SpriteRenderer>();
+      spriteRen.enabled = false;
+      var explode = Instantiate(ExplosionPrefab, transform.position, Quaternion.identity, transform.parent);
+      Invoke("Destruct", 0.1f);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+      if (collision.relativeVelocity.magnitude >= ThresholdForce) {
+        Boom();
+      }
+    }
+
+}
---
+++ Angry Blox - Peer Grading/Assets/Code/TargetBox.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class TargetBox : MonoBehaviour
+{
+    /// <summary>
+    /// Targets that move past this point score automatically.
+    /// </summary>
+    public static float OffScreen;
+
+
+    internal void Start() {
+        OffScreen = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width-100, 0, 0)).x;
+    }
+
+    internal void Update()
+    {
+        if (transform.position.x > OffScreen)
+            Scored();
+    }
+
+    bool detected = false;
+
+    private void Scored()
+    {
+          SpriteRenderer rTarget = gameObject.GetComponent<SpriteRenderer>();
+          rTarget.color = Color.green;
+          if (detected) {
+            return;
+          }
+          TargetBox tBox = gameObject.GetComponent<TargetBox>();
+          if (tBox) {
+            tBox.detected = true;
+          }
+
+          ScoreKeeper.AddToScore(gameObject.GetComponent<Rigidbody2D>().mass);
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.transform.tag == "Ground") {
+          Scored();
+        }
+    }
+}
